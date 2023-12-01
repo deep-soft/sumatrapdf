42edc3a427a92a70455ee523531a1dfd9d96ea2b
deep-soft <deep-soft@users.noreply.github.com>
2023-12-02 00:09:40 +0200

    Update bin-install-drmem.sh

--- bins/bin-install-drmem.sh
+++ bins/bin-install-drmem.sh
@@ -14,7 +14,11 @@ name_msi_full=$(find . -name "$name_msi" | xargs readlink -f)
 echo "name_msi_full=$name_msi_full" >> $GITHUB_ENV
 echo "list: pushd"
 ls -l
-msiexec /i $name_msi_full
+msiexec /i $name_msi_full /quiet /qn /norestart /log $name_msi_full-install.log
+cat $name_msi_full-install.log
 popd
 echo "list: popd"
 ls -l
+echo "run drmemory.exe"
+drmemory.exe
+echo "Done."
