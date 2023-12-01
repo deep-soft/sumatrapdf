cd8576c244b3b743a1bc7faa862b45bef953716b
deep-soft <deep-soft@users.noreply.github.com>
2023-12-01 23:50:19 +0200

    Create bin-install-drmem.sh

---
+++ .github/workflows/bin-install-drmem.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#.github/workflows/bin-common.txt
+#.github/workflows/bin-win.txt
+name_common=$(cat .github/workflows/bin-common.txt);
+name_msi=$(cat .github/workflows/bin-win.txt);
+echo "URL:$name_common$name_msi";
+#curl refs/tags/bins/$name_7z -o $name_7z;
+mkdir tmp
+pushd tmp
+echo "curl -LOJ $name_common$name_msi;"
+curl -LOJ $name_common$name_msi;
+# name_7z_full=$(find . -type f -name $name_7z -print0 | xargs -0 realpath)
+name_msi_full=$(find . -name "$name_msi" | xargs readlink -f)
+echo "name_msi_full=$name_msi_full" >> $GITHUB_ENV
+echo "list: pushd"
+ls -l
+msiexec /i $name_msi_full
+popd
+echo "list: popd"
+ls -l
